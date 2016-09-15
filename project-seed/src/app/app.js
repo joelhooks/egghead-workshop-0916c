b9f02712f5f015671a4fd8bde55d8fe649f50605
joelhooks <joelhooks@gmail.com>
2016-09-15 10:48:03 -0500

    add the initial loading of users

--- project-seed/src/app/app.js
+++ project-seed/src/app/app.js
@@ -3,8 +3,15 @@ angular.module('project-seed', [
   'ngAria',
   'ui.router',
   'ui.bootstrap',
-  'templates-app'
+  // 'templates-app'
 ])
+  .controller('AppCtrl', function AppCtrl($http) {
+    var app = this;
 
+    $http.get('https://api.github.com/users').then(function (results) {
+      app.users = results.data;
+    })
+
+  })
 
 ;
